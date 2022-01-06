5c7f43b15f0cd7653c14948f680a2b1b4db8e047
Cyrille <cyril2lambda@gmail.com>
2022-01-06 12:06:24 +0300

    Finish lab6

--- lab6/main.py
+++ lab6/main.py
@@ -1,11 +1,14 @@
 import re
 from collections import namedtuple
+from typing import Union
 
+import matplotlib.pyplot as plt
 import requests
 from bs4 import BeautifulSoup
-from bs4.element import Tag
+from bs4.element import Tag, PageElement
 from natasha import NewsNERTagger, Doc, NewsEmbedding, Segmenter, NewsMorphTagger, NewsSyntaxParser, PER, ORG
 from natasha.doc import DocSpan
+from wordcloud import WordCloud
 
 ENTRY = 'https://www.herzen.spb.ru/main/facts/nowadays/1558542985/1559574321/'
 
@@ -18,18 +21,16 @@ def main():
     entry = get_page_content(ENTRY)
     container = get_news_container(entry)
 
-    uris = map(get_href, container.children)
-    uris = filter(lambda href: href, uris)
+    uris = filter(lambda href: href, map(get_href, container.children))
 
     pages = map(lambda uri: Page(uri, get_page_content(uri)), uris)
     articles = map(lambda page: Article(page.uri, get_article_content(page.html)), pages)
-
     term_packs = map(lambda article: TermPack(article, extract_crucial_terms(article.content)), articles)
 
-    for pack in term_packs:
-        print(pack.terms)
+    wordclouds = map(generate_wordcloud, map(join, term_packs))
 
-        exit()
+    for wordcloud in wordclouds:
+        show_figure(wordcloud)
 
 
 def get_page_content(uri: str) -> str:
@@ -43,7 +44,7 @@ def get_news_container(html: str) -> Tag:
     return soup.select('img[src="/uploads/nabrinken/images/news3.jpg"]')[0].parent.parent
 
 
-def get_href(tag: Tag) -> str:
+def get_href(tag: Union[Tag, PageElement]) -> str:
     txt = str(tag.get_text)
     return parse_href(txt)
 
@@ -79,6 +80,21 @@ def extract_crucial_terms(text: str) -> list:
         for span in doc.spans  # type: DocSpan
         if span.type in [PER, ORG]
     ]
+
+
+def join(pack: TermPack) -> str:
+    return ' '.join(pack.terms)
+
+
+def generate_wordcloud(text: str) -> WordCloud:
+    return WordCloud().generate(text)
+
+
+def show_figure(wordcloud: WordCloud) -> None:
+    plt.figure()
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
 
 
 if __name__ == '__main__':
